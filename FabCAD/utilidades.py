ca411b9bf43522840f5410fab9949c9be3b9cb55
José A. Tejero Aguilar <jata.imk@hotmail.com>
2021-04-09 02:17:30 -0500

    Utilidad: Agregado metodo para extraer el string del padre de un cuerpo

--- FabCAD/utilidades.py
+++ FabCAD/utilidades.py
@@ -1,11 +1,17 @@
 #Modulo que contiene funciones para facilitar el desarrollo de el paquete FabCAD
-def extraerString(dato):    
+def extraerString(dato):
     if type(dato) is str:
         return dato
     try:
         return dato.nombre
     except AttributeError:
         return dato.Name
+
+def extraerStringPadre(dato):
+    try:
+        return dato.padre
+    except AttributeError:
+        return str(dato.Parents[0])[1:-1].split(",")[1].strip().strip("'").split(".")[0]
     
 def vincular(objeto, objetoBase):
     if objetoBase.objetoActivo:
@@ -13,4 +19,4 @@ def vincular(objeto, objetoBase):
         objetoBase.base.getObject(objetoBase.objetoActivo).addObject(objetoBase.base.getObject(objeto))
 
 #Modulos a importan cuando se haga un from xxx import __all__ (Convención)
-__all__ = ["extraerString", "vincular"]
+__all__ = ["extraerString", "extraerStringPadre", "vincular"]
